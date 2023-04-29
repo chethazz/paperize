02b707ca2d69bd3ac5e6524478f9dffa841d4f7d
Regulus <chethaskp10@gmail.com>
2023-04-29 21:09:58 +0530

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/navbar/Navbar';
 import Home from './components/header/Home';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      Credits - Unsplash
+    </div>
+  )
+}
+
+export default Footer
