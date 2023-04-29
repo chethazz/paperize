2066518e50370c41243a619a994b6eff858a70e1
Regulus <chethaskp10@gmail.com>
2023-04-29 21:40:51 +0530

    Initialize react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/navbar/Navbar';
 import Home from './components/header/Home';
 import Footer from './components/footer/Footer';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <Navbar />
