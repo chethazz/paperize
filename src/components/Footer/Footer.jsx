9cd17a7753bbc05a606f721740b8175f6197a12c
Regulus <chethaskp10@gmail.com>
2023-04-30 21:05:14 +0530

    Update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,14 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
+      <div className='credits'>
       <h2>Credits - Unsplash</h2>
+      </div>
+      <div className='regulus'>
+        <h3>By&nbsp;
+          <a href="https://chethazz.vercel.app">Regulus</a>
+        </h3>
+      </div>
     </div>
   )
 }
