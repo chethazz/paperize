adb0e0217a64644ff6af9703c4161681061d3383
Regulus <chethaskp10@gmail.com>
2023-04-30 15:40:58 +0530

    Add Mountain - Smartphone

---
+++ src/components/devices/smartphone/smartphonecat/MountainsSmartphone.jsx
@@ -0,0 +1,301 @@
+import React from "react";
+import Navbar from "../../../navbar/Navbar";
+import Footer from "../../../footer/Footer";
+import "./SmartphoneCat.css";
+import mediumZoom from "medium-zoom";
+import { useEffect, useRef } from "react";
+
+export const MountainSmartphone = () => {
+  const zoomRef = useRef(null);
+
+  useEffect(() => {
+    mediumZoom(".zoom", {
+      margin: 20,
+      scrollOffset: 120,
+      background: "rgba(0,0,0,0.8)",
+    });
+
+    zoomRef.current = mediumZoom;
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="main">
+        <div className="mountain-img category-head">
+          <h1>Mountain</h1>
+        </div>
+
+        <section class="mt-5 gridsmartphone m-auto">
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,paint"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,oil"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,indigo"
+              alt=""
+              class="zoom rounded-4 shadow-sm"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,conceptual"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,creative"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,geometric"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,minimal"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,sky"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,blue"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,weather"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,pink"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,red"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,green"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,oil paint"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,reflection"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,color blend"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,paint,background"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,flowers"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,colors"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,feathers"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="SmartphoneAbstract18 rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,dark"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,lights"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,violet"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,peach"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,amoled"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,pastel"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,sandal"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,modern"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,illusion"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,symmetry"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,curve"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,texture"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,simple"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,muted"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?mountain,black and white"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default MountainSmartphone;
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import AerialSmartphone from './components/devices/smartphone/smartphonecat/Aeri
 import ArchitectureSmartphone from './components/devices/smartphone/smartphonecat/ArchitectureSmartphone';
 import CarsSmartphone from './components/devices/smartphone/smartphonecat/CarsSmartphone';
 import CitySmartphone from './components/devices/smartphone/smartphonecat/CitySmartphone';
+import MountainSmartphone from './components/devices/smartphone/smartphonecat/MountainsSmartphone';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -24,6 +25,7 @@ root.render(
           <Route path='home/smartphone/architecture' element={<ArchitectureSmartphone />} />
           <Route path='home/smartphone/city' element={<CitySmartphone />} />
           <Route path='home/smartphone/cars' element={<CarsSmartphone />} />
+          <Route path='home/smartphone/mountain' element={<MountainSmartphone />} />
       <Route path='about' element={<About />} />
     </Routes>
   </BrowserRouter>
