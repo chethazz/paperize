e660ba4e80bc973d2791552179c9f4499aa24cf2
Regulus <chethaskp10@gmail.com>
2023-04-30 11:22:50 +0530

    Add Desktop

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import About from './components/header/About'
+import Desktop from './components/devices/desktop/Desktop'
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-   <Routes>
-    <Route path='home' element={<App />} />
-    <Route path='about' element={<About />} />
+    <Routes>
+      <Route path='/home' element={<App />} />
+      <Route path='home/desktop' element={<Desktop />} />
+      <Route path='about' element={<About />} />
     </Routes>
-    </BrowserRouter>
+  </BrowserRouter>
 );
 
