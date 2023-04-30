39959c3ce387efeb13cd7c423c66b745cd71dc3e
Regulus <chethaskp10@gmail.com>
2023-04-30 20:46:40 +0530

    Set route

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import './Navbar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+    const location = useLocation();
+    const navigateTo = (page) => {
+        navigate(`/${page}`);
+        window.scrollTo(0, 0);
+      }
   return (
     <nav>
       <ul class="m-auto p-0">
-          <li class="nav-item d-inline-block px-3">Home</li>
-          <li class="nav-item d-inline-block px-3">About</li>
+          <li class="nav-item d-inline-block px-3" onClick={() => navigateTo("home")}>Home</li>
+          <li class="nav-item d-inline-block px-3" onClick={() => navigateTo("about")}>About</li>
       </ul>
     </nav>
   )
