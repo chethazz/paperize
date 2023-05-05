0044d1f126c24e379e9883d449d28a6fd8f1b12e
Regulus <chethaskp10@gmail.com>
2023-05-05 18:54:39 +0530

    core.ignorecase false

---
+++ src/components/devices/desktop/desktopcat/MinimalDesktop.jsx
@@ -0,0 +1,301 @@
+import React from "react";
+import Navbar from "../../../navbar/Navbar";
+import Footer from "../../../footer/Footer";
+import "./DesktopCat.css";
+import mediumZoom from "medium-zoom";
+import { useEffect, useRef } from "react";
+
+export const MinimalDesktop = () => {
+  const zoomRef = useRef(null);
+
+  useEffect(() => {
+    mediumZoom(".zoom", {
+      margin: 20,
+      scrollOffset: 120,
+      background: "rgba(0,0,0,0.8)",
+    });
+
+    zoomRef.current = mediumZoom;
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="main">
+        <div className="minimal-img category-head">
+          <h1>Minimal</h1>
+        </div>
+
+        <section class="mt-5 griddesktop m-auto">
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,,"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,paint"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,minimal"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,indigo"
+              alt=""
+              class="zoom rounded-4 shadow-sm"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?simple,conceptual"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,creative"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,pastel"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?simple,minimal"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,minimalism"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimalism"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimalist"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimalist,minimal"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,dark"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimalist,simple"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,orange"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,pink"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?simple"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,color"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,flowers"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,technology"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,gadget"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimalism,creative"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimalist,peach"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,interior"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,retro"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,sky"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimalistic,beautiful"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,sun"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,gradient"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,grey"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,colorless"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,sepia"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimalism,orange"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,coffee"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,s"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/3840x2160/daily?minimal,black and white"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default MinimalDesktop;
---
+++ src/components/devices/smartphone/smartphonecat/MinimalSmartphone.jsx
@@ -0,0 +1,301 @@
+import React from "react";
+import Navbar from "../../../navbar/Navbar";
+import Footer from "../../../footer/Footer";
+import "./SmartphoneCat.css";
+import mediumZoom from "medium-zoom";
+import { useEffect, useRef } from "react";
+
+export const MinimalSmartphone = () => {
+  const zoomRef = useRef(null);
+
+  useEffect(() => {
+    mediumZoom(".zoom", {
+      margin: 20,
+      scrollOffset: 120,
+      background: "rgba(0,0,0,0.8)",
+    });
+
+    zoomRef.current = mediumZoom;
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="main">
+        <div className="minimal-img category-head">
+          <h1>Minimal</h1>
+        </div>
+
+        <section class="mt-5 gridsmartphone m-auto">
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,,"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,paint"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,minimal"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,indigo"
+              alt=""
+              class="zoom rounded-4 shadow-sm"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?simple,conceptual"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,creative"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,pastel"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?simple,minimal"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,minimalism"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimalism"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimalist"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimalist,minimal"
+              alt=""
+              class="zoom rounded-4 shadow"
+            />
+          </div>
+
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,dark"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimalist,simple"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,orange"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,pink"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?simple"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,color"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,flowers"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,technology"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,gadget"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimalism,creative"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimalist,peach"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,interior"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,retro"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,sky"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimalistic,beautiful"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,sun"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,gradient"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,grey"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,colorless"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,sepia"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimalism,orange"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,coffee"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,s"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+          <div class="rounded-4 shadow">
+            <img
+              src="https://source.unsplash.com/featured/1440x3120/daily?minimal,black and white"
+              alt=""
+              class="zoom  rounded-4 shadow"
+            />
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default MinimalSmartphone;
